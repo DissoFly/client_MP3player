88772b6da72ded15ec07a8bb7d8bfe040195dbd5
DissoCapB <280596129@qq.com>
2017-01-15 22:48:59 +0800

    第一次提交——空白文档

---
+++ app/src/main/java/com/example/mp3player/BootActivity.java
@@ -0,0 +1,13 @@
+package com.example.mp3player;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BootActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_boot);
+    }
+}
