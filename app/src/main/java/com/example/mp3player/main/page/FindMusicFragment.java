cb8e2325c2712b30d95c025b2c95ca0f1e3162d9
DissoCapB <280596129@qq.com>
2017-02-09 16:04:11 +0800

    测试Android和servlet的通信

--- app/src/main/java/com/example/mp3player/main/page/FindMusicFragment.java
+++ app/src/main/java/com/example/mp3player/main/page/FindMusicFragment.java
@@ -2,12 +2,21 @@ package com.example.mp3player.main.page;
 
 import android.app.Fragment;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.mp3player.R;
+import com.example.mp3player.service.HttpService;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Request;
+import okhttp3.Response;
+
 
 /**
  * Created by DissoCapB on 2017/1/16.
@@ -16,13 +25,49 @@ import com.example.mp3player.R;
 public class FindMusicFragment extends Fragment {
 
     View view;
+    TextView textView;
 
 
-    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        if (view==null)
-            view=inflater.inflate(R.layout.fragment_main_page_find_music,null);
+        if (view == null)
+            view = inflater.inflate(R.layout.fragment_main_page_find_music, null);
+        textView = (TextView) view.findViewById(R.id.textView);
         return view;
+    }
+
+
+    @Override
+    public void onResume() {
+
+        super.onResume();
+        reload();
+    }
+
+    void reload() {
+        Request request = HttpService.requestBuilderWithPath("text").get().build();
+        HttpService.getClient().newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(final Call call, final IOException e) {
+                textView.setText(e.toString());
+                System.out.println(e.toString());
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    final String data = response.body().string();
+
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            textView.setText(data);
+                        }
+                    });
+                } catch (Exception e) {
+
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/mp3player/service/httpService.java
@@ -0,0 +1,65 @@
+package com.example.mp3player.service;
+
+import java.net.CookieManager;
+import java.net.CookiePolicy;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import okhttp3.Cookie;
+import okhttp3.CookieJar;
+import okhttp3.HttpUrl;
+import okhttp3.JavaNetCookieJar;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by DissoCapB on 2017/2/9.
+ */
+
+public class HttpService {
+    static OkHttpClient client;
+
+    public static String serverAddress = "http://192.168.253.3:8080/app19a/";
+
+    static {
+        CookieJar cookieJar = new CookieJar() {
+            Map<HttpUrl, List<Cookie>> cookiemap = new HashMap<HttpUrl, List<Cookie>>();
+
+            @Override
+            public List<Cookie> loadForRequest(HttpUrl key) {
+                List<Cookie> cookies = cookiemap.get(key);
+                if (cookies == null) {
+                    return new ArrayList<Cookie>();
+                } else
+                    return cookies;
+            }
+
+            @Override
+            public void saveFromResponse(HttpUrl key, List<Cookie> value) {
+                cookiemap.put(key, value);
+            }
+        };
+
+        CookieManager cookieManager = new CookieManager();
+        cookieManager.setCookiePolicy(CookiePolicy.ACCEPT_ALL);
+
+
+        client = new OkHttpClient.Builder()
+                .cookieJar(new JavaNetCookieJar(cookieManager))
+                .build();
+
+        client = new OkHttpClient.Builder().cookieJar(cookieJar).cookieJar(new JavaNetCookieJar(cookieManager)).build();
+    }
+
+    public static OkHttpClient getClient() {
+        return client;
+    }
+
+    public static Request.Builder requestBuilderWithPath(String path) {
+        return new Request.Builder().url(serverAddress + "/" + path);
+    }
+
+
+}
