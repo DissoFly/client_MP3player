509d94460b625653f64cc37c98ec1afd1e2070d0
DissoCapB <280596129@qq.com>
2017-03-21 15:18:56 +0800

    完成歌曲点赞及显示功能

--- app/src/main/java/com/example/mp3player/windows/PlayingActivity.java
+++ app/src/main/java/com/example/mp3player/windows/PlayingActivity.java
@@ -17,14 +17,25 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.mp3player.R;
 import com.example.mp3player.entity.PlayingItem;
 import com.example.mp3player.service.DownloadService;
+import com.example.mp3player.service.HttpService;
+import com.example.mp3player.service.LoginService;
 import com.example.mp3player.service.MusicPlayerService;
 import com.example.mp3player.windows.inputcells.SongPictureFragment;
 
+import java.io.IOException;
 import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 
 /**
@@ -53,6 +64,7 @@ public class PlayingActivity extends Activity implements View.OnClickListener {
     TextView localOrOnline;
     Button btnDownload;
     Button btnComment;
+    Button btnLike;
 
     ImageView btnPlayChanges;
     ImageView btnPlayBack;
@@ -72,6 +84,7 @@ public class PlayingActivity extends Activity implements View.OnClickListener {
         localOrOnline = (TextView) findViewById(R.id.text_playing_local_or_online);
         btnDownload = (Button) findViewById(R.id.btn_playing_download);
         btnComment = (Button) findViewById(R.id.btn_playing_comment);
+        btnLike = (Button) findViewById(R.id.btn_playing_like);
         animation();
         getFragmentManager().beginTransaction()
                 .replace(R.id.frag_song_img, songPictureFragment).commit();
@@ -109,6 +122,7 @@ public class PlayingActivity extends Activity implements View.OnClickListener {
         findViewById(R.id.btn_play_list).setOnClickListener(this);
         findViewById(R.id.btn_playing_download).setOnClickListener(this);
         findViewById(R.id.btn_playing_comment).setOnClickListener(this);
+        findViewById(R.id.btn_playing_like).setOnClickListener(this);
     }
 
     @Override
@@ -130,37 +144,154 @@ public class PlayingActivity extends Activity implements View.OnClickListener {
                 messenger.next();
                 break;
             case R.id.btn_playing_comment:
-                CommentFragment commentFragment=new CommentFragment();
+                CommentFragment commentFragment = new CommentFragment();
                 commentFragment.setSongId(audioList.get(listPosition).getOnlineSongId());
                 getFragmentManager().beginTransaction()
                         .replace(R.id.layout_out, commentFragment).addToBackStack(null).commit();
+                break;
+            case R.id.btn_playing_like:
+                setLike();
+
                 break;
             case R.id.btn_play_list:
                 getFragmentManager().beginTransaction()
                         .replace(R.id.layout_out, new FooterPlayingListFragment()).addToBackStack(null).commit();
                 break;
             case R.id.btn_playing_download:
-                messengerDownload.downloadMusic(audioList.get(listPosition).getOnlineSongId(),audioList.get(listPosition).getSongName());
+                messengerDownload.downloadMusic(audioList.get(listPosition).getOnlineSongId(), audioList.get(listPosition).getSongName());
                 break;
             default:
                 break;
         }
     }
 
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        bindService(new Intent(this, MusicPlayerService.class), connection, Context.BIND_AUTO_CREATE);
-        bindService(new Intent(this, DownloadService.class), connectionDownload, Context.BIND_AUTO_CREATE);
-
-    }
-
     @Override
     protected void onResume() {
         super.onResume();
+        bindService(new Intent(this, MusicPlayerService.class), connection, Context.BIND_AUTO_CREATE);
+        bindService(new Intent(this, DownloadService.class), connectionDownload, Context.BIND_AUTO_CREATE);
+        bindService(new Intent(this, LoginService.class), loginConnection, Context.BIND_AUTO_CREATE);
     }
 
+    private void getLike() {
+        if (listPosition >= 0) {
+            if (audioList.get(listPosition).isOnline()) {
+                int userId = -1;
+                if (loginMessenger.getUser() != null) {
+                    userId = loginMessenger.getUser().getUserId();
+                }
+                getMusicLikeConnect(userId, audioList.get(listPosition).getOnlineSongId());
+            }
+        }
+    }
+
+    private void setLike() {
+        if (listPosition >= 0) {
+            if (audioList.get(listPosition).isOnline()) {
+                int userId = -1;
+                if (loginMessenger.getUser() != null) {
+                    userId = loginMessenger.getUser().getUserId();
+                }
+                setMusiclikeConnect(userId, audioList.get(listPosition).getOnlineSongId());
+            }
+        }
+    }
+
+    private void getMusicLikeConnect(int userId, int onlineSongId) {
+        RequestBody formBody = new FormBody.Builder()
+                .add("userId", userId + "")
+                .add("musicId", onlineSongId + "")
+                .build();
+        Request request = HttpService.requestBuilderWithPath("like/getMusicLikeNumber").post(formBody).build();
+        HttpService.getClient().newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(final Call call, final IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    final String data = response.body().string();
+                    if (data.endsWith("FALSE")) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                btnLike.setText("赞" + "(" + data.replace("FALSE", "") + ")");
+                            }
+                        });
+                    } else if (data.endsWith("TRUE")) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                btnLike.setText("已赞" + "(" + data.replace("TRUE", "") + ")");
+                            }
+                        });
+                    } else {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                btnLike.setText("连接错误");
+                            }
+                        });
+                        System.out.println(data);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private void setMusiclikeConnect(int userId, int onlineSongId) {
+        RequestBody formBody = new FormBody.Builder()
+                .add("userId", userId + "")
+                .add("musicId", onlineSongId + "")
+                .build();
+        Request request = HttpService.requestBuilderWithPath("like/setMusicLike").post(formBody).build();
+        HttpService.getClient().newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(final Call call, final IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    final String data = response.body().string();
+                    if (data.equals("SUCCESS_ADD")) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(PlayingActivity.this, "点赞成功", Toast.LENGTH_SHORT).show();
+                                getLike();
+                            }
+                        });
+                    } else if (data.equals("SUCCESS_DELECT")) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(PlayingActivity.this, "取消点赞", Toast.LENGTH_SHORT).show();
+                                getLike();
+                            }
+                        });
+                    } else {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(PlayingActivity.this, "连接错误", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                        System.out.println(data);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    int reflashSign = -1;
 
     Handler handler = new Handler();
     Runnable runnable = new Runnable() {
@@ -168,6 +299,13 @@ public class PlayingActivity extends Activity implements View.OnClickListener {
         public void run() {
             audioList = messenger.getAudioList();
             listPosition = messenger.getPlayingListPosition();
+
+            if (reflashSign != listPosition) {
+                if (loginBound) {
+                    getLike();
+                    reflashSign = listPosition;
+                }
+            }
 
             if (listPosition == -1) {
                 playingName.setText("音乐，让生活更美好");
@@ -182,16 +320,22 @@ public class PlayingActivity extends Activity implements View.OnClickListener {
                     localOrOnline.setText("本地音乐");
                     btnDownload.setEnabled(false);
                     btnComment.setEnabled(false);
+                    btnLike.setVisibility(View.GONE);
+                    btnLike.setEnabled(false);
                     btnDownload.setText("本地");
-                }else if (audioList.get(listPosition).isOnline()&&audioList.get(listPosition).isDownload()) {
+                } else if (audioList.get(listPosition).isOnline() && audioList.get(listPosition).isDownload()) {
                     localOrOnline.setText("网络下载");
                     btnDownload.setEnabled(false);
                     btnComment.setEnabled(true);
+                    btnLike.setVisibility(View.VISIBLE);
+                    btnLike.setEnabled(true);
                     btnDownload.setText("已下载");
-                }else {
+                } else {
                     localOrOnline.setText("网络在线");
                     btnDownload.setEnabled(true);
                     btnComment.setEnabled(true);
+                    btnLike.setVisibility(View.VISIBLE);
+                    btnLike.setEnabled(true);
                     btnDownload.setText("下载");
                 }
                 if (messenger.isPlaying())
@@ -279,6 +423,25 @@ public class PlayingActivity extends Activity implements View.OnClickListener {
             messenger = ((MusicPlayerService.ServiceBinder) service).getService();
             handler.postDelayed(runnable, REFLASH_TIME);
             bound = true;
+        }
+    };
+
+
+    LoginService loginMessenger;
+    boolean loginBound;
+
+    private ServiceConnection loginConnection = new ServiceConnection() {
+
+        public void onServiceDisconnected(ComponentName name) {
+            loginMessenger = null;
+            loginBound = false;
+        }
+
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            loginMessenger = ((LoginService.ServicesBinder) service).getService();
+            loginBound = true;
+            reflashSign = listPosition;
+            getLike();
         }
     };
 
